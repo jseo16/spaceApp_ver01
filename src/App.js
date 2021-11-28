f224dbefbee6c5a14b095c7bd701be4fcbd258b6
Jinok <joseo46@hotmail.com>
2021-11-28 17:33:50 +1100

    add Home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Home from './screens/Home/Home';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <main>
+        <Home />
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+function Home() {
+  return (
+    <div className={`d-flex align-items-center text-center ${styles.home}`}>
+      Home
+    </div>
+  );
+}
+
+export default Home;
